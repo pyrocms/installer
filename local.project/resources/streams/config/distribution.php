6d7481521c71435484be6c42557d4f5585f5ebf9
Ryan Thompson <ryan@pyrocms.com>
2017-01-28 19:08:54 -0600

    Pyro-ize

---
+++ local.project/resources/streams/config/distribution.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'name'        => 'PyroCMS',
+    'description' => 'The most powerful Laravel CMS in the world.',
+    'version'     => 'v3.2.2',
+];
--- src/NewCommand.php
+++ src/NewCommand.php
@@ -2,18 +2,17 @@
 
 namespace Laravel\Installer\Console;
 
-use ZipArchive;
 use RuntimeException;
-use GuzzleHttp\Client;
-use Symfony\Component\Process\Process;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
 
 class NewCommand extends Command
 {
+
     /**
      * Configure the command options.
      *
@@ -23,50 +22,51 @@ class NewCommand extends Command
     {
         $this
             ->setName('new')
-            ->setDescription('Create a new Laravel application.')
+            ->setDescription('Create a new Pyro application.')
             ->addArgument('name', InputArgument::OPTIONAL)
-            ->addOption('dev', null, InputOption::VALUE_NONE, 'Installs the latest "development" release')
-            ->addOption('5.2', null, InputOption::VALUE_NONE, 'Installs the "5.2" release');
+            ->addOption('tag', null, InputOption::VALUE_OPTIONAL, 'Installs a specific version tag of Pyro.')
+            ->addOption('dev', null, InputOption::VALUE_NONE, 'Installs Pyro with source preferred.');
     }
 
     /**
      * Execute the command.
      *
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @param  \Symfony\Component\Console\Input\InputInterface   $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface $output
      * @return void
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if (! class_exists('ZipArchive')) {
-            throw new RuntimeException('The Zip PHP extension is not installed. Please install it and try again.');
-        }
-
         $this->verifyApplicationDoesntExist(
-            $directory = ($input->getArgument('name')) ? getcwd().'/'.$input->getArgument('name') : getcwd()
+            $directory = ($input->getArgument('name')) ? getcwd() . '/' . $input->getArgument('name') : getcwd()
         );
 
-        $output->writeln('<info>Crafting application...</info>');
+        $options = '';
 
-        $version = $this->getVersion($input);
+        if ($input->getOption('dev')) {
+            $options .= ' --prefer-source';
+        }
 
-        $this->download($zipFile = $this->makeFilename(), $version)
-             ->extract($zipFile, $directory)
-             ->cleanUp($zipFile);
+        if ($tag = $input->getOption('tag')) {
+            $tag = '=' . $tag;
+        }
+
+        $output->writeln('<info>Installing Pyro...</info>');
 
         $composer = $this->findComposer();
 
         $commands = [
-            $composer.' install --no-scripts',
-            $composer.' run-script post-root-package-install',
-            $composer.' run-script post-install-cmd',
-            $composer.' run-script post-create-project-cmd',
+            $composer . ' create-project pyrocms/pyrocms' . $tag . ' ' . $directory . $options,
+            $composer . ' install',
         ];
 
         if ($input->getOption('no-ansi')) {
-            $commands = array_map(function ($value) {
-                return $value.' --no-ansi';
-            }, $commands);
+            $commands = array_map(
+                function ($value) {
+                    return $value . ' --no-ansi';
+                },
+                $commands
+            );
         }
 
         $process = new Process(implode(' && ', $commands), $directory, null, null, null);
@@ -75,17 +75,19 @@ class NewCommand extends Command
             $process->setTty(true);
         }
 
-        $process->run(function ($type, $line) use ($output) {
-            $output->write($line);
-        });
+        $process->run(
+            function ($type, $line) use ($output) {
+                $output->write($line);
+            }
+        );
 
-        $output->writeln('<comment>Application ready! Build something amazing.</comment>');
+        $output->writeln('<comment>Pyro is ready!</comment>');
     }
 
     /**
      * Verify that the application does not already exist.
      *
-     * @param  string  $directory
+     * @param  string $directory
      * @return void
      */
     protected function verifyApplicationDoesntExist($directory)
@@ -96,106 +98,14 @@ class NewCommand extends Command
     }
 
     /**
-     * Generate a random temporary filename.
-     *
-     * @return string
-     */
-    protected function makeFilename()
-    {
-        return getcwd().'/laravel_'.md5(time().uniqid()).'.zip';
-    }
-
-    /**
-     * Download the temporary Zip to the given file.
-     *
-     * @param  string  $zipFile
-     * @param  string  $version
-     * @return $this
-     */
-    protected function download($zipFile, $version = 'master')
-    {
-        switch ($version) {
-            case 'develop':
-                $filename = 'latest-develop.zip';
-                break;
-            case 'master':
-                $filename = 'latest.zip';
-                break;
-            case '5.2':
-                $filename = 'latest-52.zip';
-                break;
-        }
-
-        $response = (new Client)->get('http://cabinet.laravel.com/'.$filename);
-
-        file_put_contents($zipFile, $response->getBody());
-
-        return $this;
-    }
-
-    /**
-     * Extract the Zip file into the given directory.
-     *
-     * @param  string  $zipFile
-     * @param  string  $directory
-     * @return $this
-     */
-    protected function extract($zipFile, $directory)
-    {
-        $archive = new ZipArchive;
-
-        $archive->open($zipFile);
-
-        $archive->extractTo($directory);
-
-        $archive->close();
-
-        return $this;
-    }
-
-    /**
-     * Clean-up the Zip file.
-     *
-     * @param  string  $zipFile
-     * @return $this
-     */
-    protected function cleanUp($zipFile)
-    {
-        @chmod($zipFile, 0777);
-
-        @unlink($zipFile);
-
-        return $this;
-    }
-
-    /**
-     * Get the version that should be downloaded.
-     *
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @return string
-     */
-    protected function getVersion(InputInterface $input)
-    {
-        if ($input->getOption('dev')) {
-            return 'develop';
-        }
-
-        if ($input->getOption('5.2')) {
-            return '5.2';
-        }
-
-        return 'master';
-    }
-
-    /**
      * Get the composer command for the environment.
      *
      * @return string
      */
     protected function findComposer()
     {
-        if (file_exists(getcwd().'/composer.phar')) {
-            return '"'.PHP_BINARY.'" composer.phar';
+        if (file_exists(getcwd() . '/composer.phar')) {
+            return '"' . PHP_BINARY . '" composer.phar';
         }
 
         return 'composer';
